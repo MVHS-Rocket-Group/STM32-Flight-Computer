1c1946925a4a3462abd0541f1ee6cb091d59afd5
Neil Balch <neilbalch@gmail.com>
2020-01-30 15:21:23 -0800

    Better demo script.... :)

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,53 @@
 #include <Arduino.h>
-#include "IMU.h"
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
+#include <Arduino_LSM9DS1.h>
+#include <constants.h>
+
+#define SD_CS_PIN PB0
 
 Sd2Card card;
 SdVolume volume;
 SdFile root;
 
-#define SD_CS_PIN PB0;
-
-#define DT 0.02       // Loop time
-#define AA 0.97       // complementary filter constant
-#define G_GAIN 0.070  // [deg/s/LSB]
-
-byte buff[6];
-int accRaw[3];
-int magRaw[3];
-int gyrRaw[3];
-float rate_gyr_y = 0.0;  // [deg/s]
-float rate_gyr_x = 0.0;  // [deg/s]
-float rate_gyr_z = 0.0;  // [deg/s]
-float gyroXangle = 0.0;
-float gyroYangle = 0.0;
-float gyroZangle = 0.0;
-float AccYangle = 0.0;
-float AccXangle = 0.0;
-float CFangleX = 0.0;
-float CFangleY = 0.0;
-
-unsigned long startTime;
-
 void setup() {
-  // join i2c bus (address optional for master)
-  Serial.begin(9600);  // start serial for output
-  // while (!Serial.available()) delay(1);
-  for (uint32_t i = 0; i < 10; i++) {
+  Serial.begin(9600);
+  while (!Serial);
+  for (uint32_t i = 0; i < 5; i++) {
     Serial.print(".");
     delay(500);
   }
   Serial.println();
 
-  detectIMU();
-  enableIMU();
+  // I2C device detector
+  {
+    int count = 0;
+    Wire.begin();
+    for (byte i = 8; i < 120; i++)
+    {
+      Wire.beginTransmission (i);
+      if (Wire.endTransmission () == 0)
+        {
+        Serial.print ("Found address: ");
+        Serial.print (i, DEC);
+        Serial.print (" (0x");
+        Serial.print (i, HEX);
+        Serial.println (")");
+        count++;
+        delay (1);  // maybe unneeded?
+        } // end of good response
+    } // end of for loop
+    Serial.println ("Done.");
+    Serial.print ("Found ");
+    Serial.print (count, DEC);
+    Serial.println (" device(s).");
+  }
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
 
   // SD Card demo
   {
@@ -117,77 +122,34 @@ void setup() {
 }
 
 void loop() {
-  startTime = millis();
+  float x, y, z;
 
-  // Read the measurements from  sensors
-  readACC(buff);
-  accRaw[0] = (int)(buff[0] | (buff[1] << 8));
-  accRaw[1] = (int)(buff[2] | (buff[3] << 8));
-  accRaw[2] = (int)(buff[4] | (buff[5] << 8));
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(x, y, z);
 
-  readMAG(buff);
-  magRaw[0] = (int)(buff[0] | (buff[1] << 8));
-  magRaw[1] = (int)(buff[2] | (buff[3] << 8));
-  magRaw[2] = (int)(buff[4] | (buff[5] << 8));
+    Serial.print("acc: (g's) ");
+    Serial.print(x);
+    Serial.print(' ');
+    Serial.print(y);
+    Serial.print(' ');
+    Serial.print(z);
 
-  readGYR(buff);
-  gyrRaw[0] = (int)(buff[0] | (buff[1] << 8));
-  gyrRaw[1] = (int)(buff[2] | (buff[3] << 8));
-  gyrRaw[2] = (int)(buff[4] | (buff[5] << 8));
+    IMU.readGyroscope(x, y, z);
 
-  // Convert Gyro raw to degrees per second
-  rate_gyr_x = (float)gyrRaw[0] * G_GAIN;
-  rate_gyr_y = (float)gyrRaw[1] * G_GAIN;
-  rate_gyr_z = (float)gyrRaw[2] * G_GAIN;
+    Serial.print("\tgyro: (deg/s) ");
+    Serial.print(x);
+    Serial.print(' ');
+    Serial.print(y);
+    Serial.print(' ');
+    Serial.print(z);
 
-  // Calculate the angles from the gyro
-  gyroXangle += rate_gyr_x * DT;
-  gyroYangle += rate_gyr_y * DT;
-  gyroZangle += rate_gyr_z * DT;
+    IMU.readMagneticField(x, y, z);
 
-  // Convert Accelerometer values to degrees
-  AccXangle = (float)(atan2(accRaw[1], accRaw[2]) + M_PI) * RAD_TO_DEG;
-  AccYangle = (float)(atan2(accRaw[2], accRaw[0]) + M_PI) * RAD_TO_DEG;
-
-  // If IMU is up the correct way, use these lines
-  AccXangle -= (float)180.0;
-  if (AccYangle > 90)
-    AccYangle -= (float)270;
-  else
-    AccYangle += (float)90;
-
-  // Complementary filter used to combine the accelerometer and gyro values.
-  CFangleX = AA * (CFangleX + rate_gyr_x * DT) + (1 - AA) * AccXangle;
-  CFangleY = AA * (CFangleY + rate_gyr_y * DT) + (1 - AA) * AccYangle;
-
-  // Compute heading
-  float heading = 180 * atan2(magRaw[1], magRaw[0]) / M_PI;
-
-  // Convert heading to 0 - 360
-  if (heading < 0) heading += 360;
-
-  Serial.print("AccX ");
-  Serial.print(AccXangle);
-  Serial.print("\tAccY ");
-  Serial.print(AccYangle);
-
-  Serial.print("\tGyrX ");
-  Serial.print(gyroXangle);
-  Serial.print("\tGyrY ");
-  Serial.print(gyroYangle);
-  Serial.print("\tGyrZ ");
-  Serial.print(gyroZangle);
-  Serial.print("\tCFangleX ");
-  Serial.print(CFangleX);
-  Serial.print("\tCFangleY ");
-  Serial.print(CFangleY);
-  Serial.print("\theading ");
-  Serial.print(heading);
-  Serial.print("\tLoop Time ");
-
-  // Each loop should be at least 20ms.
-  while (millis() - startTime < (DT * 1000)) {
-    delay(1);
+    Serial.print("\tmag: (uT) ");
+    Serial.print(x);
+    Serial.print(' ');
+    Serial.print(y);
+    Serial.print(' ');
+    Serial.println(z);
   }
-  Serial.println(millis() - startTime);
 }
